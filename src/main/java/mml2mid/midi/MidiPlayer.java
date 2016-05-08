5a0abb425ab2322b91e1cfcc0f5df9583ef4c9b5
Atsushi KOSHIKIZAWA <atsushi-koshikizawa@navitime.co.jp>
2016-05-08 23:42:12 +0900

    noteon, noteoff

--- src/main/java/mml2mid/midi/MidiPlayer.java
+++ src/main/java/mml2mid/midi/MidiPlayer.java
@@ -1,9 +1,13 @@
 package mml2mid.midi;
 
+import com.google.common.collect.Lists;
+
 import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Synthesizer;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.BlockingQueue;
 
 import static com.google.common.collect.Queues.newLinkedBlockingQueue;
@@ -29,6 +33,7 @@ public class MidiPlayer {
 
     /**
      * MidiSynthesizerのコンストラクタ
+     *
      * @param play_mode 同期方法
      */
     public MidiPlayer(int play_mode) {
@@ -48,4 +53,48 @@ public class MidiPlayer {
         }
     }
 
+    private void startPlayer() {
+        player = new Thread() {
+            public void run() {
+                while (true) {
+                    try {
+                        int[] note = notes.take();
+                        if (note[0] == -1)
+                            break;
+                        for (int i = 3; i < note.length; i++) {
+                            noteOn(note[0], note[i], note[1]);
+                        }
+                        sleep(note[2]);
+                        for (int i = 3; i < note.length; i++) {
+                            noteOff(note[0], note[i]);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        break;
+                    }
+                }
+            }
+        };
+        player.start();
+    }
+
+    private synchronized void noteOn(int channel, int note, int vel) {
+        channels[channel].noteOn(note, vel);
+    }
+
+    private synchronized void noteOn(int channel, int[] notes, int vel) {
+        for(int note : notes) {
+            channels[channel].noteOn(note, vel);
+        }
+    }
+
+    private synchronized void noteOff(int channel, int note) {
+        channels[channel].noteOff(note);
+    }
+
+    private synchronized void noteOff(int channel, int[] notes) {
+        for(int note : notes) {
+            channels[channel].noteOff(note);
+        }
+    }
 }
