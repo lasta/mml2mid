35c16649c64e79fe14f05d94049ffccf1a7bb789
Atsushi KOSHIKIZAWA <atsushi-koshikizawa@navitime.co.jp>
2016-05-09 00:59:44 +0900

    MidiPlayer の実装できた
    再生テストしていない
    
    parser.Node がバグっていたのを修正した

--- src/main/java/mml2mid/midi/MidiPlayer.java
+++ src/main/java/mml2mid/midi/MidiPlayer.java
@@ -1,13 +1,9 @@
 package mml2mid.midi;
 
-import com.google.common.collect.Lists;
-
 import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Synthesizer;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.concurrent.BlockingQueue;
 
 import static com.google.common.collect.Queues.newLinkedBlockingQueue;
@@ -18,7 +14,7 @@ import static com.google.common.collect.Queues.newLinkedBlockingQueue;
 public class MidiPlayer {
     Synthesizer synthesizer;
     MidiChannel[] channels;
-    int max_channel;
+    int maxChannel;
     int playMode;
 
     // 同期的
@@ -44,7 +40,7 @@ public class MidiPlayer {
             e.printStackTrace();
         }
         channels = synthesizer.getChannels();
-        max_channel = channels.length;
+        maxChannel = channels.length;
         this.playMode = playMode;
 
         if (play_mode == QUEUED) {
@@ -95,6 +91,157 @@ public class MidiPlayer {
     private synchronized void noteOff(int channel, int[] notes) {
         for(int note : notes) {
             channels[channel].noteOff(note);
+        }
+    }
+
+    private boolean channelOk (int channel) {
+        if ((channel >= 0) && channel < maxChannel) {
+            return true;
+        } else {
+            System.err.println(
+                    "Midiチャンネル番号が不正です : " +
+                            channel +
+                            " (limit : " +
+                            maxChannel +
+                            ")"
+            );
+            return false;
+        }
+    }
+
+    /**
+     * 使用できるチャンネルの最大値を返す
+     * @return 使用できるチャンネルの最大値
+     */
+    public int getMaxChannel() {
+        return maxChannel;
+    }
+
+    /**
+     * チャンネルに楽器を設定する
+     * @param channel チャンネル番号
+     * @param bank 楽器のバンク番号
+     * @param program 楽器のプログラム番号
+     */
+    public void setChannel(int channel, int bank, int program) {
+        if(channelOk(channel)) {
+            synchronized(this) {
+                channels[channel].programChange(
+                        bank >> 1,
+                        ((bank & 1) << 7) + program
+                );
+            }
+        }
+    }
+
+    public void play(int channel, int note, int vel, int length) {
+        if (channelOk(channel)) {
+            switch (playMode) {
+                case SYNCHRONIZED:
+                    playSynchronously(channel, note, vel, length);
+                    break;
+                case ASYNCHRONIZED:
+                    playAsynchronously(channel, note, vel, length);
+                    break;
+                case QUEUED:
+                    playQueued(channel, note, vel, length);
+            }
+        }
+    }
+
+    public void play(int channel, int[] notes, int vel, int length) {
+        if (channelOk(channel)) {
+            switch (playMode) {
+                case SYNCHRONIZED:
+                    playSynchronously(channel, notes, vel, length);
+                    break;
+                case ASYNCHRONIZED:
+                    playAsynchronously(channel, notes, vel, length);
+                    break;
+                case QUEUED:
+                    playQueued(channel, notes, vel, length);
+            }
+        }
+    }
+
+    private void playSynchronously(int channel, int note, int vel, int length) {
+        noteOn(channel, note, vel);
+        try {
+            Thread.sleep(length);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        noteOff(channel, note);
+    }
+
+    private void playSynchronously(int channel, int[] note, int vel, int length) {
+        noteOn(channel, note, vel);
+        try {
+            Thread.sleep(length);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        noteOff(channel, note);
+    }
+
+    private void playAsynchronously(int channel, int note, int vel, int length) {
+        Thread player = new Thread() {
+            public void run() {
+                noteOn(channel, note, vel);
+                try {
+                    Thread.sleep(length);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                noteOff(channel, note);
+            }
+        };
+        player.start();
+        this.player = player;
+    }
+
+    private void playAsynchronously(int channel, int[] notes, int vel, int length) {
+        Thread player = new Thread() {
+            public void run() {
+                noteOn(channel, notes, vel);
+                try {
+                    Thread.sleep(length);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                noteOff(channel, notes);
+            }
+        };
+        player.start();
+        this.player = player;
+    }
+
+    private void playQueued(int channel, int note, int vel, int length) {
+        int[] val = new int[4];
+        val[0] = channel;
+        val[1] = vel;
+        val[2] = length;
+        val[4] = note;
+        try {
+            this.notes.put(val);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void playQueued(int channel, int[] notes, int vel, int length) {
+        int[] val = new int[3 + notes.length];
+        val[0] = channel;
+        val[1] = vel;
+        val[2] = length;
+        for (int i = 0; i < notes.length; i++) {
+            val[3 + i] = notes[i];
+        }
+
+        try {
+            this.notes.put(val);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
